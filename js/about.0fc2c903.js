b9ab68d140ea549d01c61b83fd828c6169554ef0
vehymy <selimhanbaltas@gmail.com>
2023-06-12 12:22:38 +0300

    adding dist

--- js/about.0fc2c903.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunkrouter1"]=self["webpackChunkrouter1"]||[]).push([[443],{364:function(n,r,u){u.r(r),u.d(r,{default:function(){return l}});var t=u(252);const e={class:"about"},a=(0,t._)("h1",null,"This is an about page",-1),s=[a];function c(n,r){return(0,t.wg)(),(0,t.iD)("div",e,s)}var o=u(744);const i={},f=(0,o.Z)(i,[["render",c]]);var l=f}}]);
-//# sourceMappingURL=about.0fc2c903.js.map
